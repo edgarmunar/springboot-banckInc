0497c717618bfc4f9c7440c0a7a4696d7c97038e
Edgar Munar <edgar_munar@hotmail.com>
2025-11-21 23:52:47 -0500

    Primer commit del proyecto BankInc

---
+++ src/main/java/com/edgar/bank_tarjetas/BankTarjetasApplication.java
@@ -0,0 +1,13 @@
+package com.edgar.bank_tarjetas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BankTarjetasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BankTarjetasApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/edgar/bank_tarjetas/BankTarjetasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.edgar.bank_tarjetas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BankTarjetasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
